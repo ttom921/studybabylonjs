7605b7044fe70bd229dd3c08f412e706aa8bcb6a
ttom921 <ttom921@hotmail.com>
2023-06-01 16:38:21 +0800

    feat: 建立單一房子

--- 02_Features/02_Village/src/app.ts
+++ 02_Features/02_Village/src/app.ts
@@ -46,15 +46,23 @@ class App {
         let camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
         let light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+
+        //
         const ground = this.buildGround(scene);
-        const box = this.buildBox(scene);
-        const roof = this.buildRoof(scene);
-        //const house = Mesh.MergeMeshes([box, roof]);
-        const house = Mesh.MergeMeshes([box, roof], true, false, null, false, true);
+        const house = this.buildHouse(2); //width fo house 1 or 2
+        // const box = this.buildBox(2);
+        // const roof = this.buildRoof(scene);
+        // //const house = Mesh.MergeMeshes([box, roof]);
+        // const house = Mesh.MergeMeshes([box, roof], true, false, null, false, true);
         return scene;
 
     }
-    private buildRoof(scene: Scene) {
+    private buildHouse(width: number = 2) {
+        const box = this.buildBox(width);
+        const roof = this.buildRoof(width);
+        return Mesh.MergeMeshes([box, roof], true, false, null, false, true);
+    }
+    private buildRoof(width: number = 2) {
         //texture
         const roofMat = new StandardMaterial("roofMat");
         roofMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/roof.jpg");
@@ -62,20 +70,33 @@ class App {
         const roof = MeshBuilder.CreateCylinder("roof", { diameter: 1.3, height: 1.2, tessellation: 3 });
         roof.material = roofMat;
         roof.scaling.x = 0.75;
-        roof.scaling.y = 2;
+        roof.scaling.y = width;
         roof.rotation.z = Math.PI / 2;
         roof.position.y = 1.22;
         return roof;
     }
-    private buildBox(scene: Scene) {
-        const boxMat = new StandardMaterial("boxMat");
-        boxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/semihouse.png");
+
+    private buildBox(width: number = 2) {
         //texture
+        const boxMat = new StandardMaterial("boxMat");
+        if (width == 2) {
+            boxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/semihouse.png");
+        } else {
+            boxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/cubehouse.png");
+        }
+        //set uv
         const faceUV = [];
-        faceUV[0] = new Vector4(0.6, 0.0, 1.0, 1.0); //rear face
-        faceUV[1] = new Vector4(0.0, 0.0, 0.4, 1.0); //front face
-        faceUV[2] = new Vector4(0.4, 0, 0.6, 1.0); //right side
-        faceUV[3] = new Vector4(0.4, 0, 0.6, 1.0); //left side
+        if (width == 2) {
+            faceUV[0] = new Vector4(0.6, 0.0, 1.0, 1.0); //rear face
+            faceUV[1] = new Vector4(0.0, 0.0, 0.4, 1.0); //front face
+            faceUV[2] = new Vector4(0.4, 0, 0.6, 1.0); //right side
+            faceUV[3] = new Vector4(0.4, 0, 0.6, 1.0); //left side
+        } else {
+            faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0);//rear side
+            faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0);//front side
+            faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0);//right side
+            faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0);//left side
+        }
         // top 4 and bottom 5 not seen so not set
         // world objects
         const box = MeshBuilder.CreateBox("box", { width: 2, faceUV: faceUV, wrap: true });
@@ -83,6 +104,7 @@ class App {
         box.position.y = 0.5;
         return box;
     }
+
     private buildGround(scene: Scene) {
         // color 
         const groundMat = new StandardMaterial("groundMat");
