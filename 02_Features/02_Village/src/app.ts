288accc67cdec884ad420d38361cf622a12bb33e
ttom921 <ttom921@hotmail.com>
2023-06-01 16:52:55 +0800

    feat: 建立多個房子

--- 02_Features/02_Village/src/app.ts
+++ 02_Features/02_Village/src/app.ts
@@ -43,19 +43,60 @@ class App {
     private _createScene(canvas: HTMLCanvasElement, engine: Engine) {
         /**** Set camera and light *****/
         let scene = new Scene(engine);
-        let camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, Vector3.Zero(), scene);
+        let camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
         let light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
+
+
         //
         const ground = this.buildGround(scene);
-        const house = this.buildHouse(2); //width fo house 1 or 2
-        // const box = this.buildBox(2);
-        // const roof = this.buildRoof(scene);
-        // //const house = Mesh.MergeMeshes([box, roof]);
-        // const house = Mesh.MergeMeshes([box, roof], true, false, null, false, true);
+        const mulithouses = this.createMultiHouse();
         return scene;
 
+    }
+    private createMultiHouse() {
+        const detached_hourse = this.buildHouse(1);
+        detached_hourse.rotation.y = -Math.PI / 16;
+        detached_hourse.position.x = -6.8;
+        detached_hourse.position.z = 2.5;
+
+        const semi_house = this.buildHouse(2);
+        semi_house.rotation.y = -Math.PI / 16;
+        semi_house.position.x = -4.5;
+        semi_house.position.z = 3;
+
+        const places = [];//each entry is an array [house type,rotation,x,z]
+        places.push([1, -Math.PI / 16, -6.8, 2.5]);
+        places.push([2, -Math.PI / 16, -4.5, 3]);
+        places.push([2, -Math.PI / 16, -1.5, 4]);
+        places.push([2, -Math.PI / 3, 1.5, 6]);
+        places.push([2, 15 * Math.PI / 16, -6.4, -1.5]);
+        places.push([1, 15 * Math.PI / 16, -4.1, -1]);
+        places.push([2, 15 * Math.PI / 16, -2.1, -0.5]);
+        places.push([1, 5 * Math.PI / 4, 0, -1]);
+        places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
+        places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5]);
+        places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7]);
+        places.push([2, Math.PI / 1.9, 4.75, -1]);
+        places.push([1, Math.PI / 1.95, 4.5, -3]);
+        places.push([2, Math.PI / 1.9, 4.75, -5]);
+        places.push([1, Math.PI / 1.9, 4.75, -7]);
+        places.push([2, -Math.PI / 3, 5.25, 2]);
+        places.push([1, -Math.PI / 3, 6, 4]);
+
+        //create instance from the first tow that were build
+        const houses = [];
+        for (let i = 0; i < places.length; i++) {
+            if (places[i][0] === 1) {
+                houses[i] = detached_hourse.createInstance("house" + i);
+            } else {
+                houses[i] = semi_house.createInstance("house" + i);
+            }
+            houses[i].rotation.y = places[i][1];
+            houses[i].position.x = places[i][2];
+            houses[i].position.z = places[i][3];
+        }
     }
     private buildHouse(width: number = 2) {
         const box = this.buildBox(width);
@@ -109,7 +150,7 @@ class App {
         // color 
         const groundMat = new StandardMaterial("groundMat");
         groundMat.diffuseColor = new Color3(0, 1, 0);
-        const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 });
+        const ground = MeshBuilder.CreateGround("ground", { width: 15, height: 16 });
         ground.material = groundMat;
         return ground;
     }
